902448ccb1699710dae9077c7468d1e572e16816
Denzil Williams <denzzilwilliams@gmail.com>
2020-04-10 18:45:33 -0500

    minor extension fixes

--- database/connection.php
+++ database/connection.php
@@ -13,4 +13,3 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
