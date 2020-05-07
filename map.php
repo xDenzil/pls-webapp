e47332bf3e8ec0dae2bae7c6aee4e37f814dcaa6
xDenzil <46894735+xDenzil@users.noreply.github.com>
2020-05-07 16:54:10 -0500

    testing

--- map.php
+++ map.php
@@ -95,87 +95,11 @@ if (isset($_GET['lat']) && isset($_GET['long'])) {
             <script>   
                 
                 if(<?php echo ($path)?>=='pinpoint'){
-                   var map;
-
-                function initMap() {
-                    var pinpoint = {
-                        lat: <?php echo ($latitude) ?>,
-                        lng: <?php echo ($longitude) ?>
-                    }
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        center: pinpoint,
-                        zoom: <?php echo ($zoom) ?>,
-                        streetViewControl: false,
-                        zoomControlOptions: {
-                            position: google.maps.ControlPosition.RIGHT_CENTER
-                        },
-                    });
-                    <?php echo ($marker) ?>
+                    alert("Pinpoint");
                 }
-                   }
-                   else {
-                      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(17.9762011, -76.7945833),
-          zoom: 12
-        });
-        var infoWindow = new google.maps.InfoWindow;
-
-          // Change this depending on the name of your PHP or XML file
-          downloadUrl('markers.php', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var id = markerElem.getAttribute('id');
-              var street = markerElem.getAttribute('street');
-              var parish = markerElem.getAttribute('parish');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
-    
-              
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = street + ', ' + parish;
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-
-              var text = document.createElement('text');
-              text.textContent = 'Lat: ' + parseFloat(markerElem.getAttribute('lat')) + ' Long: ' + parseFloat(markerElem.getAttribute('lng'));
-              infowincontent.appendChild(text);
-                
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-              });
-                marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-            });
-          });
-        }
-
-
-
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-      }
-
-      function doNothing() {}
-                   }
+                else {
+                    alert("All");
+                }
             
      
             </script>
