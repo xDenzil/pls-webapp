260d3f33395408675fcf140337ddf891a5b51dfe
xDenzil <46894735+xDenzil@users.noreply.github.com>
2020-05-07 18:03:36 -0500

    setup unrepaired button

--- actions.php
+++ actions.php
@@ -14,8 +14,12 @@ if($action =='repaired'){
  $sql_query =  "UPDATE potholes SET repaired = '1' WHERE id = '" . $id . "';";
 }
 
+if($action =='unrepaired'){
+ $sql_query =  "UPDATE potholes SET repaired = '0' WHERE id = '" . $id . "';";
+}
+
 $result = $conn->query($sql_query) or die("Failed to execute query");;
-header('Location: active.php');
+//header('Location: active.php');
 
 
 
