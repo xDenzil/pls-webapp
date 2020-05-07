ea36e480d2b8080bd487f5dab57be35760899e63
xDenzil <46894735+xDenzil@users.noreply.github.com>
2020-05-07 15:18:38 -0500

    sample code test

--- map.php
+++ map.php
@@ -88,8 +88,8 @@ if (isset($_GET['lat']) && isset($_GET['long'])) {
         <div class="dashboard-wrapper bg-primary mp">
             <div id="map"></div>
             
-            <script>
-                  var customLabel = {
+            <script>      
+                var customLabel = {
         restaurant: {
           label: 'R'
         },
@@ -100,32 +100,32 @@ if (isset($_GET['lat']) && isset($_GET['long'])) {
 
         function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng($latitude, $longitude),
+          center: new google.maps.LatLng(-33.863276, 151.207977),
           zoom: 12
         });
         var infoWindow = new google.maps.InfoWindow;
 
           // Change this depending on the name of your PHP or XML file
-          downloadUrl('markers.php', function(data) {
+          downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
             var xml = data.responseXML;
             var markers = xml.documentElement.getElementsByTagName('marker');
             Array.prototype.forEach.call(markers, function(markerElem) {
               var id = markerElem.getAttribute('id');
-              var street = markerElem.getAttribute('street');
-              var parish = markerElem.getAttribute('parish');
-              var detected = markerElem.getAttribute('detected');
+              var name = markerElem.getAttribute('name');
+              var address = markerElem.getAttribute('address');
+              var type = markerElem.getAttribute('type');
               var point = new google.maps.LatLng(
                   parseFloat(markerElem.getAttribute('lat')),
                   parseFloat(markerElem.getAttribute('lng')));
 
               var infowincontent = document.createElement('div');
               var strong = document.createElement('strong');
-              strong.textContent = street
+              strong.textContent = name
               infowincontent.appendChild(strong);
               infowincontent.appendChild(document.createElement('br'));
 
               var text = document.createElement('text');
-              text.textContent = parish
+              text.textContent = address
               infowincontent.appendChild(text);
               var icon = customLabel[type] || {};
               var marker = new google.maps.Marker({
