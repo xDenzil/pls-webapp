43bb79b53f5e5141dff8f939d7112f536d11b86a
xDenzil <46894735+xDenzil@users.noreply.github.com>
2020-05-07 12:34:45 -0500

    attempt at delete function

--- actions.php
+++ actions.php
@@ -1,5 +1,17 @@
 <?php
 
+session_start();
+include './database/connection.php';
+
+$id = $_GET['id'];
+$action = $_GET['action'];
+
+if($action == 'delete'){
+  $sql_delete = "DELETE FROM potholes WHERE id='" . $id . "';";
+}
+
+$result = $conn->query($sql);
+header('Location: active.php');
 
 
 
