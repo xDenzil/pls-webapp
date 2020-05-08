dae895650a3836c66a4be2060424e9f8f329b6a7
xDenzil <46894735+xDenzil@users.noreply.github.com>
2020-05-08 12:31:42 -0500

    Update map.php

--- map.php
+++ map.php
@@ -89,103 +89,105 @@ if (isset($_GET['lat']) && isset($_GET['long'])) {
         <!-- ============================================================== -->
         <div class="dashboard-wrapper bg-primary mp">
             <div id="map"></div>
-            
-            
-            
-            <script>   
-                var path = '<?php echo ($path)?>';
-                if(path == 'pinpoint'){
+
+
+
+            <script>
+                var path = '<?php echo ($path) ?>';
+                if (path == 'pinpoint') {
                     var map;
 
-                function initMap() {
-                    var pinpoint = {
-                        lat: <?php echo ($latitude) ?>,
-                        lng: <?php echo ($longitude) ?>
-                    }
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        center: pinpoint,
-                        zoom: <?php echo ($zoom) ?>,
-                        streetViewControl: false,
-                        zoomControlOptions: {
-                            position: google.maps.ControlPosition.RIGHT_CENTER
-                        },
-                    });
-                    <?php echo ($marker) ?>
-                }
-                }
-                else {
                     function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(<?php echo ($latitude) ?>, <?php echo ($longitude) ?>),
-          zoom: <?php echo ($zoom) ?>,
-                        streetViewControl: false,
-                        zoomControlOptions: {
-                            position: google.maps.ControlPosition.RIGHT_CENTER
-                        },
-        });
-        var infoWindow = new google.maps.InfoWindow;
+                        var pinpoint = {
+                            lat: <?php echo ($latitude) ?>,
+                            lng: <?php echo ($longitude) ?>
+                        }
+                        map = new google.maps.Map(document.getElementById('map'), {
+                            center: pinpoint,
+                            zoom: <?php echo ($zoom) ?>,
+                            streetViewControl: false,
+                            zoomControlOptions: {
+                                position: google.maps.ControlPosition.RIGHT_CENTER
+                            },
+                        });
+                        <?php echo ($marker) ?>
+                    }
+                } else {
+                    function initMap() {
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            center: new google.maps.LatLng(<?php echo ($latitude) ?>, <?php echo ($longitude) ?>),
+                            zoom: <?php echo ($zoom) ?>,
+                            streetViewControl: false,
+                            zoomControlOptions: {
+                                position: google.maps.ControlPosition.RIGHT_CENTER
+                            },
+                        });
+                        var infoWindow = new google.maps.InfoWindow;
 
-          // Change this depending on the name of your PHP or XML file
-          downloadUrl('markers.php', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var id = markerElem.getAttribute('id');
-              var street = markerElem.getAttribute('street');
-              var parish = markerElem.getAttribute('parish');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
-    
-              
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = street + ', ' + parish;
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-
-              var text = document.createElement('text');
-              text.textContent = 'Lat: ' + parseFloat(markerElem.getAttribute('lat')) + ' Long: ' + parseFloat(markerElem.getAttribute('lng'));
-              infowincontent.appendChild(text);
-                
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-              });
-                marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-            });
-          });
-        }
+                        // Change this depending on the name of your PHP or XML file
+                        downloadUrl('markers.php', function(data) {
+                            var xml = data.responseXML;
+                            var markers = xml.documentElement.getElementsByTagName('marker');
+                            Array.prototype.forEach.call(markers, function(markerElem) {
+                                var id = markerElem.getAttribute('id');
+                                var street = markerElem.getAttribute('street');
+                                var parish = markerElem.getAttribute('parish');
+                                var point = new google.maps.LatLng(
+                                    parseFloat(markerElem.getAttribute('lat')),
+                                    parseFloat(markerElem.getAttribute('lng')));
 
 
+                                var infowincontent = document.createElement('div');
+                                var strong = document.createElement('strong');
+                                strong.textContent = street + ', ' + parish;
+                                infowincontent.appendChild(strong);
+                                infowincontent.appendChild(document.createElement('br'));
 
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
+                                var text = document.createElement('text');
+                                text.textContent = 'Lat: ' + parseFloat(markerElem.getAttribute('lat')) + ' Long: ' + parseFloat(markerElem.getAttribute('lng'));
+                                infowincontent.appendChild(text);
 
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
+                                var markers=[];
 
-        request.open('GET', url, true);
-        request.send(null);
-      }
+                                var marker = new google.maps.Marker({
+                                    map: map,
+                                    position: point,
+                                });
 
-      function doNothing() {}
+                                marker.addListener('click', function() {
+                                    infoWindow.setContent(infowincontent);
+                                    infoWindow.open(map, marker);
+                                });
+                                markers.push(marker);
+                            });
+                        });
+                        var markerCluster = new MarkerClusterer(map, markers);
+
+                    }
+
+
+
+                    function downloadUrl(url, callback) {
+                        var request = window.ActiveXObject ?
+                            new ActiveXObject('Microsoft.XMLHTTP') :
+                            new XMLHttpRequest;
+
+                        request.onreadystatechange = function() {
+                            if (request.readyState == 4) {
+                                request.onreadystatechange = doNothing;
+                                callback(request, request.status);
+                            }
+                        };
+
+                        request.open('GET', url, true);
+                        request.send(null);
+                    }
+
+                    function doNothing() {}
                 }
-            
-     
             </script>
             <script src="https://unpkg.com/@google/markerclustererplus@4.0.1/dist/markerclustererplus.min.js"></script>
-            <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCAKrYd2b-ceSJOqO-ejr1R1c2qXB51SaM&callback=initMap"></script>
+            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCAKrYd2b-ceSJOqO-ejr1R1c2qXB51SaM&callback=initMap"></script>
 
 
             <!-- CONTAINER END -->
