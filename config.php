fc46fad5bf9e24ae26d9fea32d5186fee846e650
Denzil Williams <denzzilwilliams@gmail.com>
2020-07-08 10:37:19 -0500

    env set up

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 session_start();
 $MAPS_API_KEY = 'AIzaSyCAKrYd2b-ceSJOqO-ejr1R1c2qXB51SaM';
 
-if (getenv('MODE') != 'PRODUCTION') {
+if (getenv('ENV_MODE') != 'PRODUCTION') {
     require_once __DIR__ . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
