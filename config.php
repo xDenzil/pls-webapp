ad099f57d35eb529cd9db6bd6a0b6dab21a590fb
Denzil Williams <denzzilwilliams@gmail.com>
2020-07-08 10:29:24 -0500

    modes

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ $MAPS_API_KEY = 'AIzaSyCAKrYd2b-ceSJOqO-ejr1R1c2qXB51SaM';
 
 require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-if (getenv('PRODUCTION') == false) {
+if (getenv('MODE') == 'PRODUCTION') {
     $dotenv->load();
 }
