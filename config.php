e003eea353e55c7917cc754788a781283d335de6
Denzil Williams <denzzilwilliams@gmail.com>
2020-07-08 10:32:57 -0500

    mode fixes

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 session_start();
 $MAPS_API_KEY = 'AIzaSyCAKrYd2b-ceSJOqO-ejr1R1c2qXB51SaM';
 
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-if (getenv('MODE') == 'PRODUCTION') {
+if (getenv('MODE') != 'PRODUCTION') {
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
